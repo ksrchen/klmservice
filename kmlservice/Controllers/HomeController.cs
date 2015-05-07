9f7dfae998836bad9f3cba415508631c5f0feadf
kevin chen <kchen@loopnet.com>
2015-05-07 13:37:27 -0700

    add export

--- kmlservice/Controllers/HomeController.cs
+++ kmlservice/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,18 +10,62 @@ namespace kmlservice.Controllers
     [Authorize]
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public ActionResult Index(int dateFilter=0, bool csv=false)
         {
             using (var db = new ResIncomeEntities())
             {
+                DateTimeOffset start = DateTimeOffset.Now;
+
+                switch(dateFilter)
+                {
+                    case 0:
+                        start = start.AddDays(-1);
+                        break;
+                    case 1:
+                        start = start.AddDays(-7);
+                        break;
+                    case 2:
+                        start = start.AddMonths(-1);
+                        break;
+                    case 3:
+                        start = start.AddMonths(-3);
+                        break;
+                    case 4:
+                        start = start.AddMonths(-6);
+                        break;
+
+                }
+
                 var query = from a in db.Audits
                             join u in db.Users on a.UserID equals u.UserID
                             where a.Controller == "ResIncome" && 
-                            a.Action == "Get" 
+                            a.Action == "Get" &&
+                            a.Created >= start 
                             orderby a.Created descending 
                             select new Record { @User = u.FirstName + " " + u.LastName, @Email = u.UserID,  @MLSNumber = a.ID, @Created = a.Created };
-                            
-                return View(query.ToList());
+
+                if (csv)
+                {
+                    StringBuilder sb = new StringBuilder();
+                    sb.AppendLine("sep=|");
+                    sb.AppendLine("User|Email|MLS #|Date");
+                    foreach (var item in query)
+                    {
+                        sb.AppendFormat("{0}|{1}|{2}|{3}\r\n",
+                            item.User,
+                            item.Email,
+                            item.MLSNumber,
+                            item.Created
+                        );
+                    }
+                    var content = sb.ToString();
+                    return File(Encoding.UTF8.GetBytes(content), "application/vnd.ms-excel");
+                    
+                }
+                else
+                {
+                    return View(query.ToList());
+                }
             }
         }
     }
---
+++ kmlservice/Controllers/LoadDataController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace kmlservice.Controllers
+{
+     [Authorize]
+    public class LoadDataController : Controller
+    {
+        // GET: LoadData
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
