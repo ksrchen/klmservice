eb02cdbd69e0ec04e37e0e52f77f2e14d1847d2d
kevin chen <kchen@loopnet.com>
2015-05-08 15:45:10 -0700

    add property profile

---
+++ kmlservice/Controllers/PropertyProfileController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace kmlservice.Controllers
+{
+    public class PropertyProfileController : Controller
+    {
+        // GET: PropertyProfile
+        public ActionResult Index(string id)
+        {
+            using (var db = new ResIncomeEntities())
+            {
+                var item = db.ResIncomes.FirstOrDefault(p => p.MLnumber == id);
+                if (item != null)
+                {
+                    return View(item);
+
+                }
+                else
+                {
+                    return View("NotFound");
+                }
+            }
+        }
+    }
+}
